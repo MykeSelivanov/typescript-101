b802755ec5bdcdbd42309e1f2acb3ec7f6d273fc
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-30 22:31:06 -0500

    TS basics

--- 00-TS-Basics/app.ts
+++ 00-TS-Basics/app.ts
@@ -1,1 +1,9 @@
-console.log("Hello TS!");
+function add(num1, num2) {
+  return num1 + num2;
+}
+
+const number1 = 5;
+const number2 = 2.8;
+
+const result = add(number1, number2);
+console.log(`result`, result);
