08426ae9a71177b94db84271e5bd44b0fd7992cb
MykeSelivanov <michael777selivanov@gmail.com>
2021-09-30 23:31:42 -0500

    TS basics

--- 00-TS-Basics/app.ts
+++ 00-TS-Basics/app.ts
@@ -1,6 +1,6 @@
-function add(num1: number, num2: number, showResult: boolean) {
+function add(num1: number, num2: number, showResult: boolean, phrase: string) {
     if (showResult) {
-        console.log(num1 + num2);
+        console.log(phrase + num1 + num2);
     } else {
         return num1 + num2;
     }
@@ -9,6 +9,6 @@ function add(num1: number, num2: number, showResult: boolean) {
 const number1 = 5;
 const number2 = 2.8;
 const printResult = true;
+const resultPhrase = 'Result is: ';
 
-const result = add(number1, number2, printResult);
-console.log(`result`, result);
+add(number1, number2, printResult, resultPhrase);
