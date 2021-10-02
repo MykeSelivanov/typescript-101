ad286618fd28c67eed316a92e35917d59dc5d792
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-01 19:36:26 -0500

    object types

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -1,19 +1,6 @@
-function add(num1: number, num2: number, showResult: boolean, phrase: string) {
-    // checking types in vanilla JS
-    // if (typeof num1 !== 'number' || typeof num2 !== 'number') {
-    //     throw new Error('Incorrect input!');
-    // }
-    const result = num1 + num2;
-    if (showResult) {
-        console.log(phrase + result);
-    } else {
-        return num1 + num2;
-    }
-}
+const person = {
+  name: "Dave",
+  age: 35,
+};
 
-const number1 = 5;
-const number2 = 2.8;
-const printResult = true;
-const resultPhrase = 'Result is: ';
-
-add(number1, number2, printResult, resultPhrase);
+console.log(person.nickname);
