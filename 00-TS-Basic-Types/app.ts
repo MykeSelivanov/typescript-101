3e192bd7abfddfc474de0885b8abd9296a2416ea
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-01 19:43:24 -0500

    object types

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -1,6 +1,12 @@
+// const person: {
+//   name: string;
+//   age: number;
+// } = {
+
 const person = {
   name: "Dave",
   age: 35,
+  hobbies: []
 };
 
-console.log(person.nickname);
+// console.log(person.nickname);
