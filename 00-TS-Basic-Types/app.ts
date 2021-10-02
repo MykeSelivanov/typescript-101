2322525d93bb24944fded07f1e18a48d649084ef
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-02 18:29:36 -0500

    tuples

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -3,15 +3,18 @@ const person: {
   age: number;
   hobbies: string[];
   // tuple is a special construct typescript understands: special array with defined length and strict types for each element
-  role: [number, string];
+  role: [number, string, boolean];
 } = {
   name: 'Dave',
   age: 35,
   hobbies: ['Fishing', 'Cooking'],
-  role: [2, 'author'],
+  role: [2, 'author', true],
 };
 
-person.role.push('admin');
+// person.role[1] = 10; // this line will complain, since it expects a string
+// person.role = []; // this line will complain as well, since expected length should be 3
+// person.role.push('admin'); // unfortunately push() is an exception, which is allowed in tuples, so this line will not complain
+// length is enforced with explicit assignments, except the push() exception
 
 let favoriteActivities: string[];
 favoriteActivities = ['Sports'];
