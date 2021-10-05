6b514b1bb3ab4bf7038e6b38a5506a7a2fccef12
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-04 23:53:05 -0500

    literal types

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -18,7 +18,7 @@ console.log(`combinedNames`, combinedNames);
 // Literal types are the types, which are based on the core types, but you then have a specific version of that type
 function combine2(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
     let result;
-    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
+    if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-numbe') {
         result = +input1 + +input2;
     } else {
         result = input1.toString() + input2.toString();
