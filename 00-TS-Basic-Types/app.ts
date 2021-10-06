f9fa65915fb2cd97cd4b92623922051ed302205c
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-05 23:37:34 -0500

    type aliases

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -27,8 +27,10 @@ function combine2(input1: number | string, input2: number | string, resultConver
 }
 
 // Type Aliases
-type Combinable = number;
-function combine3(input1: number | string, input2: number | string, resultConversion: 'as-number' | 'as-text') {
+type Combinable = number | string;
+type ConversionDescriptor = 'as-number' | 'as-text';
+
+function combine3(input1: Combinable, input2: Combinable, resultConversion: ConversionDescriptor) {
     let result;
     if (typeof input1 === 'number' && typeof input2 === 'number' || resultConversion === 'as-number') {
         result = +input1 + +input2;
