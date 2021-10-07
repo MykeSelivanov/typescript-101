f8108d1ed35a4207ad721d11e8e943814a79d152
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-06 20:14:12 -0500

    func return types

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -1,16 +1,21 @@
+// number return type
 function add(n1: number, n2: number) {
 	return n1 + n2;
 }
 
+// void return type
 function printResult(num: number): void {
 	console.log('Result: ' + num);
 }
 
 console.log(printResult(add(5, 7))); // undefined
 
+// undefined return type
 function printResult2(num: number): undefined {
 	console.log('Result: ' + num);
-    return;
+	return;
 }
+
+let combnieValues: any;
 
 
