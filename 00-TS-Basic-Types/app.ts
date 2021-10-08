0a1cf7736d61b9c8c3b5c0c9da40e19229bfd2e6
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-07 19:56:02 -0500

    functiion types and callbacks

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -19,9 +19,18 @@ function printResult2(num: number): undefined {
 // Function types are the types that describe a function, regarding parameters and the return value
 let combnieValues: (a: number, b: number) => number;
 combnieValues = add;
-console.log(combnieValues(8,8));
+console.log(combnieValues(8, 8));
 
-function addAndHandle(n1: number, n2: number, callbackFunc: (num: number) => void) {
-    const result = n1 + n2;
-    callbackFunc(result);
+// You can also use function types in callbacks
+function addAndHandle(
+	n1: number,
+	n2: number,
+	callbackFunc: (num: number) => void
+) {
+	const result = n1 + n2;
+	callbackFunc(result);
 }
+
+addAndHandle(10, 20, (result) => {
+	console.log(`result`, result);
+});
