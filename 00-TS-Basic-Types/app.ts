725dd463fe2af7e6aa5992070f9dc945ad59340b
MykeSelivanov <michael777selivanov@gmail.com>
2021-10-08 20:20:59 -0500

    unknow type

--- 00-TS-Basic-Types/app.ts
+++ 00-TS-Basic-Types/app.ts
@@ -1,6 +1,13 @@
+// unknown is more restrictive than 'any' type
 let userInput: unknown;
 let userName: string;
 
 userInput = 5;
 userInput = 'Peter';
-userName = userInput;
+
+// this one would work, if userInput was of type 'any'
+// userName = userInput;
+// so we need to do an extra type check for unkonw types
+if (typeof userInput === 'string') {
+	userName = userInput;
+}
